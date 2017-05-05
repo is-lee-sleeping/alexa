630dbed6af56cbe4929d92b16fd9b026cbb19c04
Christopher Tumminello <christopher.tumminello@ey.com>
2017-05-05 11:23:20 -0400

    Initial skill with yes/no response

---
+++ index.js
@@ -0,0 +1,32 @@
+const Alexa = require('alexa-sdk');
+const https = require('https');
+
+exports.handler = (event, context, callback) => {
+  const alexa = Alexa.handler(event, context);
+  const sleeping = "Yes, Lee is sleeping";
+  const notSleeping = "No, Lee is not sleeping, somebody has to fix queequeg"
+  const options = {
+    host: 'api.isleesleeping.com',
+    path: '/',
+    port: 443,
+  };
+
+  const handlers = {
+    'isSleeping': function () {
+      const req = https.get(options, (res) => {
+        res.on('data', (data) => {
+          payload = JSON.parse(data.toString());
+
+          this.emit(':tell', payload.leeIsSleeping ? sleeping : notSleeping);
+        });
+      });
+
+      req.on('error', (err) => {
+        console.log(err);
+      });
+    }
+  };
+
+  alexa.registerHandlers(handlers);
+  alexa.execute();
+};
---
+++ sleeping.js
@@ -0,0 +1,24 @@
+module.exports = {
+  "session": {
+    "sessionId": "SessionId.b3d2b294-e474-48b7-926c-8b7092673ba4",
+    "application": {
+      "applicationId": "amzn1.ask.skill.e8380288-fb59-4c9c-be27-a40bbcd56e94"
+    },
+    "attributes": {},
+    "user": {
+      "userId": "amzn1.ask.account.AGOSTSLMTUDN44RBAXCYGFABYG5UGDDVUP4I6534FLNG7JIXBRCRYNYFI4N5ZBVJSYTIQWXEO34ZDSSY2WTIV67FZNLAN7LJITVLRR4PFTOHD2JXGIUAITWASBXHUDEINTHAHXRYL7RKAYJOKXVOXE46RSVWQRHRTLW3JLJVO4IZZM75XUTDDJWXMPVREFCNGWHFMT2BRADXLUA"
+    },
+    "new": true
+  },
+  "request": {
+    "type": "IntentRequest",
+    "requestId": "EdwRequestId.d30e6ee2-bedc-44bc-9da3-e6a41fee1364",
+    "locale": "en-US",
+    "timestamp": "2017-05-02T22:23:27Z",
+    "intent": {
+      "name": "isSleeping",
+      "slots": {}
+    }
+  },
+  "version": "1.0"
+}
